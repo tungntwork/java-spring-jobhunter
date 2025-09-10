768a38b2a4e0d37954e14200ed13d6150151c7fc
Nguyen Thanh Tung <tungnguyenthanh710@gmail.com>
2025-09-10 21:15:50 +0700

    create user

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -0,0 +1,28 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user/create")
+    public User createNewUser(
+            @RequestBody User postManUser) {
+
+        User newUser = this.userService.handleCreateUser(postManUser);
+
+        return newUser;
+    }
+}
