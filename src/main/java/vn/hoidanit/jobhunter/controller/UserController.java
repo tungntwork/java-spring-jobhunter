f881c5d495808778647ef5066042f89bfa8d8566
Nguyen Thanh Tung <tungnguyenthanh710@gmail.com>
2025-09-10 21:37:40 +0700

    delete user

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,6 +1,8 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +19,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user/create")
-    public User createNewUser(
-            @RequestBody User postManUser) {
-
-        User newUser = this.userService.handleCreateUser(postManUser);
-
-        return newUser;
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "delete user";
     }
 }
